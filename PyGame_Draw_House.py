8caab2617b59a4db768de49a0cb10eb49baf6b73
DmFors <dmforsyt@gmail.com>
2021-01-20 23:09:24 +0500

    Реализовал draw_house_window, draw_house_window_frame, convert_point.
    Реализовал функцию создания окна. Реализовал функцию создания перегородок на окне. Добавил в функцию создания окна выбор расположения окна в стене по декартовым четвертям.
    Также реализовал функцию для конвертации опорной точки прямоугольника в  более удобную форму.

--- PyGame_Draw_House.py
+++ PyGame_Draw_House.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.draw import *
+
 pygame.init()
 
 
@@ -7,7 +8,7 @@ def main():
     a, b, heading = 500, 500, 'Drawing House with PyGame'
     sc = prepare_for_drawing(a, b, heading)
 
-    x, y, width, height = 250, 500, 499, 400
+    x, y, width, height = 250, 500, 250, 400
     draw_house(sc, x, y, width, height)
 
     fps = 30
@@ -53,6 +54,8 @@ def draw_house(sc, x, y, width, height):
     height_wall = 0.6 * height
     draw_house_walls(sc, x_wall, y_wall, width_wall, height_wall)
 
+    draw_house_window(sc, x_wall, y_wall, width_wall, height_wall, 1)
+
     x_roof = x
     y_roof = y - (height_fnd + height_wall)
     width_roof = width
@@ -71,9 +74,10 @@ def draw_house_foundation(sc, x, y, width, height):
     :param height: высота фундамента
     :return: None
     """
-    start_point = x - width // 2 - width % 2, y - height
+    start_point = convert_point(x, y, width, height)
+    end_point = width, height
     color = 'grey'
-    rect(sc, color, (start_point, (width, height)))
+    rect(sc, color, (start_point, end_point))
 
 
 def draw_house_walls(sc, x, y, width, height):
@@ -87,10 +91,68 @@ def draw_house_walls(sc, x, y, width, height):
     :param height: высота стены
     :return: None
     """
-    start_point = x - width // 2 - width % 2, y - height
+    start_point = convert_point(x, y, width, height)
+    end_point = width, height
     color = 'brown'
     width_wall = 1
-    rect(sc, color, (start_point, (width, height)), width_wall)
+    rect(sc, color, (start_point, end_point), width_wall)
+
+
+def draw_house_window(sc, x_wall, y_wall, width_wall, height_wall, quarter):
+    """
+    Рисует окно дома в опорной точке (x, y).
+    За опорную точку взята нижняя срединная точка одного из 4 сегментов стены дома.
+    Сегменты поделены и пронумерованы по правилам четвертей декартовой системы координат.
+    :param sc: экран для вывода рисунка
+    :param x_wall: расстояние от левой границы экрана программы до опорной точки стены
+    :param y_wall: расстояние от верхней границы экрана программы до опорной точки стены
+    :param width_wall: ширина окна
+    :param height_wall: высота окна
+    :param quarter: четверть декартовой системы координат
+    :return: None
+    """
+    if quarter > 2:
+        y_window = y_wall
+        if quarter == 3:
+            x_window = x_wall - width_wall // 4
+        else:
+            x_window = x_wall + width_wall // 4
+    else:
+        y_window = y_wall - height_wall // 2
+        if quarter == 2:
+            x_window = x_wall - width_wall // 4
+        else:
+            x_window = x_wall + width_wall // 4
+
+    n, m = 0.6, 0.8  # window proportions relative to the quarter
+    width_window = n * (width_wall // 2)
+    height_window = m * (height_wall // 2)
+
+    y_window -= (1 - m) / 2 * (width_wall // 2)
+
+    start_point = convert_point(x_window, y_window, width_window, height_window)
+    end_point = width_window, height_window
+    color = 'blue'
+    rect(sc, color, (start_point, end_point))
+    color = 'black'
+    rect(sc, color, (start_point, end_point), 4)
+
+    N = 2  # count_of_separators
+    draw_house_window_frame(sc, start_point, width_window, height_window, N)
+
+
+def draw_house_window_frame(sc, start_point, width, height, N):
+    x_tmp, y_tmp = start_point
+    dx = width / (N + 1)
+    for _ in range(N):
+        x_tmp += dx
+        line(sc, 'black', (x_tmp, y_tmp), (x_tmp, y_tmp + height), 2)
+
+    x_tmp, y_tmp = start_point
+    dy = height / (N + 1)
+    for _ in range(N):
+        y_tmp += dy
+        line(sc, 'black', (x_tmp, y_tmp), (x_tmp + width, y_tmp), 2)
 
 
 def draw_house_roof(sc, x, y, width, height):
@@ -106,7 +168,7 @@ def draw_house_roof(sc, x, y, width, height):
     """
     left_point = x - width // 2 - width % 2, y
     middle_point = x, y - height
-    right_point = x + width // 2    , y
+    right_point = x + width // 2, y
     point_list = [left_point, middle_point, right_point]
     color = 'orange'
     polygon(sc, color, point_list)
@@ -126,6 +188,18 @@ def run_program(fps):
                 pygame.quit()
                 quit()
         clock.tick(fps)
+
+
+def convert_point(x, y, width, height):
+    """
+    Возвращает левую верхнюю точку прямоугольника по полученной опорной точке
+    :param x: координата опорной точки
+    :param y: координата опорной точки
+    :param width: ширина прямоугольника
+    :param height: высота прямоуголника
+    :return: левую верхнюю точку прямоугольника
+    """
+    return x - width // 2 - width % 2, y - height
 
 
 main()
