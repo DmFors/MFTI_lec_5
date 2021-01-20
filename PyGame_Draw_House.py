1b4b0dfa31f724c9c85729f76cf754d3d66b38e7
DmFors <dmforsyt@gmail.com>
2021-01-20 17:08:36 +0500

    Update PyGame_Draw_House.py
    1. Изменил main функцию. Добавил функции prepare_for_drawing, run_program.
    2.  prepare_for_drawing: Сочинил реализацию функции создания и возврата экрана программы, в котором будет производится рисование объектов.
    3.  run_program: Сочинил  функцию запуска и поддержания исполнения программы.
    
    Требуется реализовать функции run_program, draw_house_foundation, draw_house_walls, draw_house_roof

--- PyGame_Draw_House.py
+++ PyGame_Draw_House.py
@@ -1,72 +1,111 @@
+import pygame
+from pygame.draw import *
+
+
 def main():
+    a, b, heading = 600, 1200, 'Drawing House with PyGame'
+    sc = prepare_for_drawing(a, b, heading)
+
     x, y, width, height = 100, 100, 200, 400
-    draw_house(x, y, width, height)
+    draw_house(sc, x, y, width, height)
+
+    fps = 30
+    run_program(fps)
 
 
-def draw_house(x, y, width, height):
+def prepare_for_drawing(a, b, heading):
+    """
+    Возвращает экран программы размером a * b
+    Задаёт заголовок программы на heading
+    :param a: ширина экрана
+    :param b: высота экрана
+    :param heading: заголовок программы
+    :return: экран программы, на котором будет осуществляться рисование
+    """
+    sc = pygame.display.set_mode((a, b))
+    pygame.display.set_caption(heading)
+    return sc
+
+
+def draw_house(sc, x, y, width, height):
     """
     Рисует дом в опорной точке (x, y).
     За опорную точку взята нижняя серединная точка фундамента.
-    :param x: Расстояние от левой границы экрана программы до опорной точки
-    :param y: Расстояние от верхней границы экрана программы до опорной точки
-    :param width: Абсолютная ширина дома
-    :param height: Абсолютная высота дома
+    :param sc: экран для вывода рисунка
+    :param x: расстояние от левой границы экрана программы до опорной точки
+    :param y: расстояние от верхней границы экрана программы до опорной точки
+    :param width: абсолютная ширина дома
+    :param height: абсолютная высота дома
     :return: None
     """
+
     x_fnd = x
     y_fnd = y
     width_fnd = width
     height_fnd = 0.1 * height
-    draw_house_foundation(x_fnd, y_fnd, width_fnd, height_fnd)
+    draw_house_foundation(sc, x_fnd, y_fnd, width_fnd, height_fnd)
 
     x_wall = x
     y_wall = y + height_fnd
     width_wall = 0.9 * width
     height_wall = 0.6 * height
-    draw_house_walls(x_wall, y_wall, width_wall, height_wall)
+    draw_house_walls(sc, x_wall, y_wall, width_wall, height_wall)
 
     x_roof = x
     y_roof = y_wall + height_wall
     width_roof = width
     height_roof = height - height_wall - height_fnd
-    draw_house_roof(x_roof, y_roof, width_roof, height_roof)
+    draw_house_roof(sc, x_roof, y_roof, width_roof, height_roof)
 
 
-def draw_house_foundation(x, y, width, height):
+def draw_house_foundation(sc, x, y, width, height):
     """
     Рисует фундамент дома в опорной точке (x, y).
     За опорную точку взята нижняя серединная точка фундамента.
-    :param x: Расстояние от левой границы экрана программы до опорной точки
-    :param y: Расстояние от верхней границы экрана программы до опорной точки
-    :param width: Ширина фундамента
-    :param height: Высота фундамента
+    :param sc: экран для вывода рисунка
+    :param x: расстояние от левой границы экрана программы до опорной точки
+    :param y: расстояние от верхней границы экрана программы до опорной точки
+    :param width: ширина фундамента
+    :param height: высота фундамента
     :return: None
     """
-    pass
+    start_point = x - width // 2, y - height
+    color = 'grey'
 
 
-def draw_house_walls(x, y, width, height):
+def draw_house_walls(sc, x, y, width, height):
     """
     Рисует стену дома в опорной точке (x, y).
     За опорную точку взята нижняя серединная точка фундамента.
-    :param x: Расстояние от левой границы экрана программы до опорной точки
-    :param y: Расстояние от верхней границы экрана программы до опорной точки
-    :param width: Ширина стены
-    :param height: Высота стены
+    :param sc: экран для вывода рисунка
+    :param x: расстояние от левой границы экрана программы до опорной точки
+    :param y: расстояние от верхней границы экрана программы до опорной точки
+    :param width: ширина стены
+    :param height: высота стены
     :return: None
     """
     pass
 
 
-def draw_house_roof(x, y, width, height):
+def draw_house_roof(sc, x, y, width, height):
     """
     Рисует крышу дома в опорной точке (x, y).
     За опорную точку взята нижняя серединная точка фундамента.
-    :param x: Расстояние от левой границы экрана программы до опорной точки
-    :param y: Расстояние от верхней границы экрана программы до опорной точки
-    :param width: Ширина крыши
-    :param height: Высота крыши
+    :param sc: экран для вывода рисунка
+    :param x: расстояние от левой границы экрана программы до опорной точки
+    :param y: расстояние от верхней границы экрана программы до опорной точки
+    :param width: ширина крыши
+    :param height: высота крыши
     :return: None
+    """
+    pass
+
+
+def run_program(fps):
+    """
+    Запускает программу, окно которой обновляется fps кадров в секунду
+    :param fps: количество сменяемых кадров в секунду
+    :return:
     """
     pass
 
